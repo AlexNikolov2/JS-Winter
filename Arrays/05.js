4484225f4003699ddb31f592aab2cc5749449bcf
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-04 23:15:39 +0200

    add in first

--- Arrays/05.js
+++ Arrays/05.js
@@ -1,19 +1,23 @@
 let people = ["Greg", "Mary", "Devon", "James"];
 
-function getAll(){
-    for (const iterator of people) {
-        console.log(iterator);
-    }
+function getAll() {
+  for (const iterator of people) {
+    console.log(iterator);
+  }
 }
 
-
-function removeFirst(){
-    people.shift();
-    console.log(people);
+function removeFirst() {
+  people.shift();
+  console.log(people);
 }
 
-function removeLast(){
-    people.pop();
-    console.log(people);
+function removeLast() {
+  people.pop();
+  console.log(people);
 }
-removeLast();
+
+function addFirst() {
+  people.unshift("Matt");
+  console.log(people);
+}
+addFirst();
