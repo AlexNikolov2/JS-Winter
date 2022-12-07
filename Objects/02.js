4d6903b79f9de72a01b8ad29ad94d065386eebab
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-08 01:23:23 +0200

    02

---
+++ Objects/02.js
@@ -0,0 +1,12 @@
+function deleteProperty() {
+  var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12,
+  };
+
+  delete student.rollno;
+
+  console.log(student);
+}
+deleteProperty();
