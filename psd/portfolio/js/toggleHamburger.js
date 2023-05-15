89a78f591e6a63769296ab813829fb3de3620e84
Aleks Nikolov <tcr.official.world@gmail.com>
2023-05-15 16:55:03 +0300

    toggle navbar done

--- psd/portfolio/js/toggleHamburger.js
+++ psd/portfolio/js/toggleHamburger.js
@@ -1,4 +1,16 @@
 function toggleHamburger() {
   let navbar = document.getElementById("navbar");
   let burger = document.getElementById("burger");
+  let navbarLinks = document.getElementById("navbar-links");
+
+  burger.addEventListener("click", () => {
+    if (navbarLinks.style.display === "flex") {
+      navbarLinks.style.display = "none";
+      navbarLinks.style.flexDirection = "row";
+    } else {
+      navbarLinks.style.display = "flex";
+      navbar.style.flexDirection = "column-reverse";
+      navbarLinks.style.flexDirection = "column";
+    }
+  });
 }
