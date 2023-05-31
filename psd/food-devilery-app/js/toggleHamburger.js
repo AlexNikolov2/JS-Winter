4d80431b0e21058d90d933060fc0146c20da2200
Aleks Nikolov <tcr.official.world@gmail.com>
2023-06-01 01:49:13 +0300

    top navbar

--- psd/food-devilery-app/js/toggleHamburger.js
+++ psd/food-devilery-app/js/toggleHamburger.js
@@ -1,1 +1,23 @@
-function toggleHamburger() {}
+function toggleHamburger() {
+  let navbar = document.getElementById("navbar");
+  let burger = document.getElementById("burger");
+  let navbarLinks = document.getElementById("navbar-links");
+
+  burger.addEventListener("click", () => {
+    if (navbarLinks.style.display === "flex") {
+      navbarLinks.style.display = "none";
+      navbarLinks.style.flexDirection = "row";
+      burger.classList.remove("toggled");
+      burger.classList.add("untoggled");
+    } else {
+      navbarLinks.style.display = "flex";
+      navbarLinks.style.flexDirection = "column";
+      navbarLinks.style.width = "100%";
+      navbarLinks.style.alignItems = "center";
+      navbarLinks.style.justifyContent = "center";
+      navbar.style.flexDirection = "column-reverse";
+      burger.classList.add("toggled");
+      burger.classList.remove("untoggled");
+    }
+  });
+}
